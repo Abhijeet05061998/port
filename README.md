92204c818fa8cdcc804d45632e632818f87a91ad
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-01-06 01:05:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# port
+
+## Available Scripts
+
+To start this project on your localhost.
+
+### `npm i && npm start`
+
+
+## Tech stack
+
+- React
+- MaterialUI
+- Bootstrap
+- react-pdf
+- react-router
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./layout/Header";
+import Content from "./layout/Content";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
